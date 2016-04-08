5b54bb2bcd01bcfc80b6d41f7cd6dadf77759985
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-04-08 14:26:12 -0400

    Create Get-ServiceCatalogRO.ps1

---
+++ CiresonPortalPS/Public/Get-ServiceCatalogRO.ps1
@@ -0,0 +1,14 @@
+function Get-ServiceCatalogRO
+{
+<#
+.EXAMPLE
+    Get-ServiceCatalogRO -SearchText "Other Request" -SearchType All
+#>
+PARAM(
+$SearchText,
+[ValidateSet("Incidents","Requests","Favorites","All")]
+$SearchType
+)
+    $URL = $CiresonPortalURL,"/V3/ServiceCatalog/Search?searchText=$SearchText&searchType=$SearchType&skipCount=0&takeCount=0" -join '/'
+    Invoke-RestMethod $url -Credential $CiresonPortalCred
+}
