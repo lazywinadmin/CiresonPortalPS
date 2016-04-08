f4ed5254c4cd36f5fb17101dc8a92f9f5503ea87
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-04-08 14:29:48 -0400

    Create Get-CiresonPortalAnalyst.ps1

---
+++ CiresonPortalPS/Public/Get-CiresonPortalAnalyst.ps1
@@ -0,0 +1,11 @@
+function Get-CiresonPortalAnalyst
+{
+<#
+.EXAMPLE
+    Get-CiresonPortalAnalyst
+#>
+PARAM()
+    # User who are Analyst
+    $URI = $CiresonPortalURL,"/V3/User/GetAnalystResults" -join '/'
+    Invoke-RestMethod $URI -Credential $CiresonPortalCred
+}
