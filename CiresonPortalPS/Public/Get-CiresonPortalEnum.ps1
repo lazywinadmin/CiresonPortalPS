754443d0b6312fcb1fa2d50a368faaa90f5ae0ad
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-04-20 17:45:15 -0400

    Update Get-CiresonPortalEnum.ps1

--- CiresonPortalPS/Public/Get-CiresonPortalEnum.ps1
+++ CiresonPortalPS/Public/Get-CiresonPortalEnum.ps1
@@ -1,20 +1,49 @@
 function Get-CiresonPortalEnum
 {
 <#
-GetList retrieves the children of an enumeration class id that is a parent. Returns items that are only one level down from the root id that is sent.
-.EXAMPLE
-    # ServiceRequestPriorityEnum
-    Get-CiresonPortalEnum -ClassID 'd55e65ea-fae9-f7db-0937-843bfb1367c0' -ItemFilter ""
+	.SYNOPSIS
+		Function to retrieve the item(s) of a specified Enum class
+	
+	.DESCRIPTION
+		Function to retrieve the item(s) of a specified Enum class
+	
+	.PARAMETER ClassID
+		Specifies the Class ID. the value must be a GUID.
+	
+	.PARAMETER ItemFilter
+		Specifies the text string to search
+	
+	.PARAMETER Flatten
+		Whether to Flatten the display names, default is false
+	
+	.EXAMPLE
+		# Retrieve the Activity Stage
+		Get-CiresonPortalEnum -ClassID 'f05ea0f0-bd02-143e-2b74-303609750328'
+	
+	.EXAMPLE
+		# Retrieve the Service Request Priority
+		Get-CiresonPortalEnum -ClassID 'd55e65ea-fae9-f7db-0937-843bfb1367c0' -ItemFilter ""
+	
+	.NOTES
+		Francois-Xavier	Cat
+		www.lazywinadmin.com
+		@lazywinadm
+		github.com/lazywinadmin
 #>
-PARAM(
-    [parameter(Mandatory)]
-    $ClassID,
-    $ItemFilter,
-    [switch]$Flatten)
-
-    $Argument = "id=$ClassID"
-    IF($PSBoundParameters['ItemFilter']){$Argument+="&itemFilter=$ItemFilter"}
-    IF($PSBoundParameters['Flatten']){$Argument+="&Flatten=$($Flatten.ToString().ToLower())"}
-    $URI = $CiresonPortalURL,"/V3/Enum/GetList?$Argument" -join '/'
-    Invoke-RestMethod $URI -Credential $CiresonPortalCred
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[guid]$ClassID,
+		
+		[string]$ItemFilter,
+		
+		[switch]$Flatten
+	)
+	
+	$Argument = "id=$ClassID"
+	IF ($PSBoundParameters['ItemFilter']) { $Argument += "&itemFilter=$ItemFilter" }
+	IF ($PSBoundParameters['Flatten']) { $Argument += "&Flatten=$($Flatten.ToString().ToLower())" }
+	$URI = $CiresonPortalURL, "/V3/Enum/GetList?$Argument" -join '/'
+	(Invoke-RestMethod $URI -Credential $CiresonPortalCred) -as [pscustomobject]
 }
