c320bc1efb1ba4eeb1ff5f04ce3cd7871ae8f369
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-04-20 15:16:27 -0400

    Update and rename Connect-CiresonPortal.ps1 to Set-CiresonPortalConnection.ps1

--- CiresonPortalPS/Public/Set-CiresonPortalConnection.ps1
+++ CiresonPortalPS/Public/Set-CiresonPortalConnection.ps1
@@ -1,11 +1,14 @@
-function Connect-CiresonPortal
+function Set-CiresonPortalConnection
 {
 <#
 	.SYNOPSIS
-		Function to create variables used to connect to Cireson Portal API
+		Function to configure the connection with the Cireson Portal API
 	
 	.DESCRIPTION
-		Function to create variables used to connect to Cireson Portal API
+		Function to configure the connection with the Cireson Portal API.
+		This function will basically store the Credential and the API URI into two variables:
+			-> CiresonPortalCred
+			-> CiresonPortalURL
 	
 	.PARAMETER Credential
 		Specifies the credential to use
@@ -14,7 +17,10 @@ function Connect-CiresonPortal
 		Specifies the URL of the CiresonPortal
 	
 	.EXAMPLE
-		Connect-CiresonPortal -URL "http://CiresonPortal.local" -Credential $cred
+		Set-CiresonPortalConnection -URL "http://CiresonPortal.local/api" -Credential $cred
+		
+	.EXAMPLE
+		Set-CiresonPortalConnection -URL "http://CiresonPortal.local/api" -Credential (Get-Credential)
 	
 	.NOTES
 		Francois-Xavier Cat
