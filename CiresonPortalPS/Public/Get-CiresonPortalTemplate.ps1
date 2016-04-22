a916e836a6ddabb7bd17e2a755d6b88f7c98deca
François-Xavier Cat <lazywinadmin@users.noreply.github.com>
2016-04-22 16:39:05 -0400

    Update Get-CiresonPortalTemplate.ps1

--- CiresonPortalPS/Public/Get-CiresonPortalTemplate.ps1
+++ CiresonPortalPS/Public/Get-CiresonPortalTemplate.ps1
@@ -8,10 +8,14 @@ function Get-CiresonPortalTemplate
     @lazywinadm
     github.com/lazywinadmin
 #>
-    [Cmdletbinding()]
+    [Cmdletbinding(DefaultParameterSetName="All")]
     PARAM(
-        [parameter(Mandatory)]
-        [Guid]$ClassID
+        [parameter(Mandatory,ParameterSetName="ClassID")]
+        [Guid]$ClassID,
+
+        [parameter(Mandatory,ParameterSetName="Type")]
+        [ValidateSet('ServiceRequest','IncidentRequest','Activity','ChangeRequest')]
+        $Type
     )
 
     BEGIN
@@ -28,11 +32,28 @@ function Get-CiresonPortalTemplate
 	}
     PROCESS
     {
-        # Build the Query
-        $URI = $CiresonPortalURL, "api/V3/Template/GetTemplates?classId=$ClassID" -join '/'
-        Write-Verbose -Message $(New-ScriptMessage -Block PROCESS -message $URI)
-            
+        IF($PSCmdlet.ParameterSetName -eq 'ClassID')
+        {
+            # Build the Query
+            $URI = $CiresonPortalURL, "api/V3/Template/GetTemplates?classId=$ClassID" -join '/'
+            Write-Verbose -Message $(New-ScriptMessage -Block PROCESS -message $URI)
+        }
+        ELSEIF($PSCmdlet.ParameterSetName -eq 'Type')
+        {
+            Switch ($Type)
+            {
+                'ServiceRequest' { $Class = "04b69835-6343-4de2-4b19-6be08c612989"}
+                'IncidentRequest' {$Class = "a604b942-4c7b-2fb2-28dc-61dc6f465c68"}
+                'Activity' {$Class = "42642d4f-d342-3f1b-965c-628a0f4119e2"}
+                'ChangeRequest' {$Class = "e6c9cf6e-d7fe-1b5d-216c-c3f5d2c7670c"}
+            }
+            # Build the Query
+            $URI = $CiresonPortalURL, "api/V3/Template/GetTemplates?classId=$Class" -join '/'
+            Write-Verbose -Message $(New-ScriptMessage -Block PROCESS -message $URI)
+        }
+
         # Invoke the Query
 	    (Invoke-RestMethod $URI -Credential $CiresonPortalCred) -as [PSCustomObject]
+
     }
 }
