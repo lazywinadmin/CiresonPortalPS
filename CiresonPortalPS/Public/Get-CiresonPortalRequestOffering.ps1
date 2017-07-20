83495a38ad0a47d6183239ff7adca8d4068389d9
François-Xavier Cat <fxcat@lazywinadmin.com>
2017-07-19 22:07:41 -0400

    Add Pre-Checks and Try/Catch

--- CiresonPortalPS/Public/Get-CiresonPortalRequestOffering.ps1
+++ CiresonPortalPS/Public/Get-CiresonPortalRequestOffering.ps1
@@ -3,14 +3,18 @@
 <#
 .SYNOPSIS
     Function to retrieve a specific Request Offering
+
 .DESCRIPTION
     Function to retrieve a specific Request Offering
+
 .PARAMETER RequestOfferingID
     Specifies the Request Offering ID (GUID)
+
 .EXAMPLE
     Get-CiresonPortalRequestOffering -RequestOfferingID 4c2e9de8-c70b-c2df-b0d0-a9820391d294
+
 .NOTES
-    Francois-Xavier Cat
+    Francois-Xavier	Cat
     lazywinadmin.com
     @lazywinadm
     github.com/lazywinadmin
@@ -21,6 +25,28 @@ PARAM(
     [parameter(Mandatory)]
     [GUID]$RequestOfferingID
 )
-    $URI = $CiresonPortalURL,"api/V3/ServiceCatalog/GetByRequestOfferingId?requestOfferingId=$RequestOfferingID" -join '/'
-    Invoke-RestMethod $URI -Credential $CiresonPortalCred
+    BEGIN
+	{
+		TRY{
+			Write-Verbose -Message $(New-ScriptMessage -Block BEGIN -message 'Checking Pre-Requisites')
+			[void](Get-CiresonPortalPSConfiguration -WarningAction Stop)
+		}
+		CATCH
+		{
+			# Stop the function
+			Throw "Not Connected to Cireson Portal"
+		}
+	}
+    PROCESS
+    {
+        TRY{
+            Write-Verbose -Message $(New-ScriptMessage -Block PROCESS -message 'Build URI')
+            $URI = $CiresonPortalURL,"api/V3/ServiceCatalog/GetByRequestOfferingId?requestOfferingId=$RequestOfferingID" -join '/'
+            Write-Verbose -Message $(New-ScriptMessage -Block PROCESS -message 'Query API')
+            Invoke-RestMethod $URI -Credential $CiresonPortalCred
+        }
+        CATCH{
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
 }
